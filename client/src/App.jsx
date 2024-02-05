93eb7008c2b030c481b66011ac276427a64ab4c8
manminder singh <bewithme2407@gmail.com>
2024-02-05 20:58:09 +0530

    created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,33 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import SighIn from './pages/SighIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className=' from-cyan-400 bg-slate-300 text-green-400'>app</div>
-    </>
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SighIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+
+
+
+      </Routes>
+
+
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/SighIn.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SighIn() {
+  return (
+    <div>SighIn</div>
+  )
+}
