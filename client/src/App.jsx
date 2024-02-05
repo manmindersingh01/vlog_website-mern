cd040c59e12a183784925c6aa222c5918c20327d
manmindersingh01 <manmindersingh@manminders-MacBook-Air.local>
2024-02-05 20:32:03 +0530

    install basic files

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' from-cyan-400 bg-slate-300 text-green-400'>app</div>
+    </>
+  )
+}
+
+export default App
